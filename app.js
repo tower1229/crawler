6bb2bbc3057fa54c94a443c4b4a9d5740d38ea54
tower1229 <tower1229@qq.com>
2017-06-09 14:35:19 +0800

    bug fix

--- app.js
+++ app.js
@@ -3,35 +3,26 @@ const fs = require('fs');
 const cheerio = require('cheerio');
 const request = require('request');
 
-const startId = "620719";
-const articalSavePath = "./data";
-const imgSavePath = "./img";
-const fetchLimit = process.argv[2] || 50;
-
-//获取下一篇文章id
+const startId = "620719"; //起始ID
+const articalSavePath = "./data"; //文章存放路径
+const imgSavePath = "./img"; //图片存放路径
+const fetchLimit = process.argv[2] || 50; //抓取条数
+//创建所需文件夹
+if (!fs.existsSync(articalSavePath)) {
+  fs.mkdirSync(articalSavePath);
+}
+if (!fs.existsSync(imgSavePath)) {
+  fs.mkdirSync(imgSavePath);
+}
+//计数器
 let fetched = 0;
+//获取关联文章id
 let syncLast = function(_csrf, op) {
-  let syncLastApi = {
-    host: 'www.cnbeta.com',
-    path: '/comment/read',
-    header: {
-      "Accept": "application/json, text/javascript, */*; q=0.01",
-      "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4",
-      "Cache-Control": "no-cache",
-      "DNT": 1,
-      "Host": "www.cnbeta.com",
-      "Pragma": "no-cache",
-      //"Referer":http://www.cnbeta.com/articles/tech/620719.htm,
-      //"Cookie":_csrf=543c10a85adaf371248716b0985d4f7a29e570c032c4c10b2c9b8cfeaaf442d1a%3A2%3A%7Bi%3A0%3Bs%3A5%3A%22_csrf%22%3Bi%3A1%3Bs%3A32%3A%22Ojjc9pBn37ASZkfvxBVvAagrprsA3AJw%22%3B%7D; PHPSESSID=lcdf05d43gqo6shsuh70ov50bl,
-      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3080.5 Safari/537.36"
-    }
-  };
   let syncUrl = 'http://www.cnbeta.com/comment/read';
   return new Promise(function(resolve, reject) {
     if (!_csrf || !op) {
       return reject(`syncLast() param error: _csrf: ${_csrf}, op: ${op}`);
     } else {
-      //syncLastApi.path = syncLastApi.path + '?_csrf=' + encodeURIComponent(_csrf) + '&op=' + encodeURIComponent(op);
       syncUrl += '?_csrf=' + encodeURIComponent(_csrf) + '&op=' + encodeURIComponent(op);
       http.get(syncUrl, function(res) {
         let resChunk = '';
@@ -48,46 +39,30 @@ let syncLast = function(_csrf, op) {
             reject(e);
           }
         });
-
       });
     }
-
   });
 };
 //保存内容
 let savedContent = function($, news_title) {
-  if (!fs.existsSync(articalSavePath)) {
-    fs.mkdirSync(articalSavePath);
-  }
   $('.article-content p').each(function(index, item) {
     let x = $(this).text().trim();
     if (x) {
       x = '  ' + x + '\n';
+      fs.appendFile('./data/' + news_title + '.txt', x, 'utf-8', function(err) {
+        if (err) {
+          console.log(err);
+        }
+      });
     }
-    fs.appendFile('./data/' + news_title + '.txt', x, 'utf-8', function(err) {
-      if (err) {
-        console.log(err);
-      }
-    });
   });
 };
 //保存图片
 let savedImg = function($, news_title) {
-  if (!fs.existsSync(imgSavePath)) {
-    fs.mkdirSync(imgSavePath);
-  }
   $('.article-content img').each(function(index, item) {
-    let img_title = $(this).parent().next().text().trim().replace(/\//g, '-');
-    if (img_title.length > 30) {
-      img_title = img_title.slice(0, 30);
-    }
-    if (!img_title) {
-      img_title = "Null";
-    }
-    const img_filename = img_title + '.jpg';
-    const img_src = $(this).attr('src'); //获取图片的url
-
-    request(img_src).pipe(fs.createWriteStream(imgSavePath + '/' + news_title + '---' + img_filename));
+    let img_src = $(this).attr('src');
+    let img_filename = news_title + '---' + index + img_src.match(/\.[^.]+$/)[0];
+    request(img_src).pipe(fs.createWriteStream(imgSavePath + '/' + img_filename));
   });
 };
 //抓取新闻
@@ -109,7 +84,6 @@ let fetchPage = function(x, fullpath) {
     }
     let html = '';
     res.setEncoding('utf-8');
-
     res.on('data', function(chunk) {
       html += chunk;
     });
@@ -117,9 +91,11 @@ let fetchPage = function(x, fullpath) {
       if (html) {
         fetched++;
         const $ = cheerio.load(html);
-
         const time = $('.cnbeta-article .title .meta span:first-child').text().trim();
         let news_title = $('.cnbeta-article .title h1').text().trim().replace(/\//g, '-');
+        if (news_title.length > 40) {
+          news_title = news_title.slice(0, 40);
+        }
         savedContent($, news_title);
         savedImg($, news_title);
         console.log(`got: ${news_title} url: ${articalUrl}`);
@@ -137,7 +113,6 @@ let fetchPage = function(x, fullpath) {
         console.log('fetchPage() failed. ', articalUrl);
       }
     });
-
   }).on('error', function(err) {
     console.log(err);
   });
